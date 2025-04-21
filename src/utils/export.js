8d3b8a2589cf37623d35925a07c891bb8fa1c4f9
rjrolon <rolonrodneyjesus@gmail.com>
2025-04-21 13:09:43 -0300

    Delete src directory

--- src/utils/export.js
+++
@@ -1,8 +0,0 @@
-import * as XLSX from 'xlsx';
-
-export const exportToExcel = (data, filename) => {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Deudas');
-  XLSX.writeFile(wb, `${filename}.xlsx`);
-};
